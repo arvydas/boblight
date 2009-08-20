0ea3ecf95ccef31ea2c7d970d9b67c0e21948b60
bob.loosen@gmail.com <bob.loosen@gmail.com@c8956a1e-8086-11de-bc80-f7d40095c7a4>
2009-08-20 23:50:32 +0000

    factored out flag stuff to a different file for future reuse
    git-svn-id: http://boblight.googlecode.com/svn/trunk@45 c8956a1e-8086-11de-bc80-f7d40095c7a4

--- src/clients/boblight-constant.cpp
+++ src/clients/boblight-constant.cpp
@@ -25,13 +25,11 @@
 
 #include "config.h"
 #include "util/misc.h"
+#include "flags.h"
 
 using namespace std;
 
 void PrintHelpMessage();
-bool ParseBoblightFlags(int argc, char *argv[], vector<string>& options, int& priority, string& address, int& port, bool& list, bool& help);
-void ListBoblightOptions();
-bool ParseBoblightOptions(void* boblight, vector<string>& options);
 
 int main(int argc, char *argv[])
 {
@@ -155,143 +153,4 @@ void PrintHelpMessage()
   cout << "  -o  add libboblight option, syntax: [light:]option=value\n";
   cout << "  -l  list libboblight options\n";
   cout << "\n";
-}
-
-bool ParseBoblightFlags(int argc, char *argv[], vector<string>& options, int& priority, string& address, int& port, bool& list, bool& help)
-{
-  string option;
-  int c;
-
-  opterr = 0; //we don't want to print error messages
-  
-  while ((c = getopt (argc, argv, "p:s:o:lh")) != -1)
-  {
-    if (c == 'p') //priority
-    {
-      option = optarg;
-      if (!StrToInt(option, priority) || priority < 0 || priority > 255)
-      {
-        PrintError("Wrong option " + static_cast<string>(optarg) + " for argument -p");
-        return false;
-      }
-    }
-    else if (c == 's') //address[:port]
-    {
-      option = optarg;
-      address = option.substr(0, option.find(':'));
-
-      if (option.find(':') != string::npos) //check if we have a port
-      {
-        option = option.substr(option.find(':') + 1);
-        string word;
-        if (!StrToInt(option, port) || port < 0 || port > 65535)
-        {
-          PrintError("Wrong option " + static_cast<string>(optarg) + " for argument -s");
-          return false;
-        }
-      }
-    }
-    else if (c == 'o') //option for libboblight
-    {
-      options.push_back(optarg);
-    }
-    else if (c == 'l') //list libboblight options
-    {
-      list = true;
-    }
-    else if (c == 'h') //print help message
-    {
-      help = true;
-    }
-    else if (c == '?')
-    {
-      if (optopt == 'p' || optopt == 's' || optopt == 'o')
-      {
-        string error = "Option ";
-        error += static_cast<char>(optopt);
-        error += " requires an argument";
-        PrintError(error);
-        return false;
-      }
-    }
-  }
-
-  return true;
-}
-
-//go through all options and print the descriptions to stdout
-void ListBoblightOptions()
-{
-  void* boblight = boblight_init();
-  int nroptions = boblight_getnroptions(boblight);
-
-  for (int i = 0; i < nroptions; i++)
-  {
-    cout << boblight_getoptiondescript(boblight, i) << "\n";
-  }
-
-  boblight_destroy(boblight);
-}
-
-bool ParseBoblightOptions(void* boblight, vector<string>& options)
-{
-  int nrlights = boblight_getnrlights(boblight);
-  
-  for (int i = 0; i < options.size(); i++)
-  {
-    string option = options[i];
-    string lightname;
-    string optionname;
-    string optionvalue;
-    int    lightnr = -1;
-
-    //check if we have a lightname, otherwise we use all lights
-    if (option.find(':') != string::npos)
-    {
-      lightname = option.substr(0, option.find(':'));
-      if (option.find(':') == option.size() - 1) //check if : isn't the last char in the string
-      {
-        PrintError("wrong option \"" + option + "\", syntax is [light:]option=value");
-        return false;
-      }
-      option = option.substr(option.find(':') + 1); //shave off the lightname
-
-      //check which light this is
-      bool lightfound = false;
-      for (int j = 0; j < nrlights; j++)
-      {
-        if (lightname == boblight_getlightname(boblight, j))
-        {
-          lightfound = true;
-          lightnr = j;
-          break;
-        }
-      }
-      if (!lightfound)
-      {
-        PrintError("light \"" + lightname + "\" used in option \"" + options[i] + "\" doesn't exist");
-        return false;
-      }
-    }
-
-    //check if '=' exists and it's not at the end of the string
-    if (option.find('=') == string::npos || option.find('=') == option.size() - 1)
-    {
-      PrintError("wrong option \"" + option + "\", syntax is [light:]option=value");
-      return false;
-    }
-
-    optionname = option.substr(0, option.find('='));
-    optionvalue = option.substr(option.find('=') + 1);
-
-    option = optionname + " " + optionvalue;
-
-    if (!boblight_setoption(boblight, lightnr, option.c_str()))
-    {
-      PrintError(boblight_geterror(boblight));
-      return false;
-    }
-  }
-
-  return true;
 }
