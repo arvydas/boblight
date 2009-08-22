ce37eab7d5c9c3cd1ae047a493096752ff1eb7f9
bob.loosen@gmail.com <bob.loosen@gmail.com@c8956a1e-8086-11de-bc80-f7d40095c7a4>
2009-08-22 15:28:54 +0000

    added: non-honored max for some options
    git-svn-id: http://boblight.googlecode.com/svn/trunk@52 c8956a1e-8086-11de-bc80-f7d40095c7a4

--- src/lib/options.h
+++ src/lib/options.h
@@ -16,13 +16,13 @@
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-//              name           type   min              max                default variable         post-process
-BOBLIGHT_OPTION(speed,         float, 0.0,             100.0,             100.0,  m_speed,         m_speed = Clamp(m_speed, 0.0, 100.0); send = true;)
-BOBLIGHT_OPTION(autospeed,     bool,  -1,              -1,                0,      m_autospeed,     send = true;)
-BOBLIGHT_OPTION(interpolation, bool,  -1,              -1,                0,      m_interpolation, send = true;)
-BOBLIGHT_OPTION(use,           bool,  -1,              -1,                0,      m_use,           send = true;)
-BOBLIGHT_OPTION(saturation,    float, 0.0,             -1,                1.0,    m_saturation,    m_saturation = Max(m_saturation, 0.0);)
-BOBLIGHT_OPTION(value,         float, 0.0,             -1,                1.0,    m_value,         m_value = Max(m_value, 0.0);)
-BOBLIGHT_OPTION(valuemin,      float, 0.0,             1.0,               0.0,    m_valuerange[0], m_valuerange[0] = Clamp(m_valuerange[0], 0.0, m_valuerange[1]);)
-BOBLIGHT_OPTION(valuemax,      float, 0.0,             1.0,               1.0,    m_valuerange[1], m_valuerange[1] = Clamp(m_valuerange[0], m_valuerange[0], 1.0);)
-BOBLIGHT_OPTION(threshold,     int,   0,               255,               0,      m_threshold,     m_threshold = Clamp(m_threshold, 0, 255);)
+//              name           type    min             max                default variable         post-process
+BOBLIGHT_OPTION(speed,         float,  0.0,            100.0,             100.0,  m_speed,         m_speed = Clamp(m_speed, 0.0, 100.0); send = true;)
+BOBLIGHT_OPTION(autospeed,     bool,  -1,             -1,                 0,      m_autospeed,     send = true;)
+BOBLIGHT_OPTION(interpolation, bool,  -1,             -1,                 0,      m_interpolation, send = true;)
+BOBLIGHT_OPTION(use,           bool,  -1,             -1,                 0,      m_use,           send = true;)
+BOBLIGHT_OPTION(saturation,    float,  0.0,            20.0,              1.0,    m_saturation,    m_saturation = Max(m_saturation, 0.0);)
+BOBLIGHT_OPTION(value,         float,  0.0,            20.0,              1.0,    m_value,         m_value = Max(m_value, 0.0);)
+BOBLIGHT_OPTION(valuemin,      float,  0.0,            1.0,               0.0,    m_valuerange[0], m_valuerange[0] = Clamp(m_valuerange[0], 0.0, m_valuerange[1]);)
+BOBLIGHT_OPTION(valuemax,      float,  0.0,            1.0,               1.0,    m_valuerange[1], m_valuerange[1] = Clamp(m_valuerange[0], m_valuerange[0], 1.0);)
+BOBLIGHT_OPTION(threshold,     int,    0,              255,               0,      m_threshold,     m_threshold = Clamp(m_threshold, 0, 255);)
