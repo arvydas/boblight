c86c270d1b385e28f35ec747979ca80eb605f12e
bob.loosen@gmail.com <bob.loosen@gmail.com@c8956a1e-8086-11de-bc80-f7d40095c7a4>
2009-10-06 22:43:05 +0000

    added: boblight-v4l sourcefiles
    git-svn-id: http://boblight.googlecode.com/svn/trunk@129 c8956a1e-8086-11de-bc80-f7d40095c7a4

---
+++ src/clients/boblight-v4l/boblight-v4l.cpp
@@ -0,0 +1,34 @@
+/*
+ * boblight
+ * Copyright (C) Bob  2009 
+ * 
+ * boblight is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * boblight is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#define BOBLIGHT_DLOPEN
+#include "lib/libboblight.h"
+
+#include "util/misc.h"
+
+int main(int argc, char *argv[])
+{
+  //load the boblight lib, if it fails we get a char* from dlerror()
+  char* boblight_error = boblight_loadlibrary(NULL);
+  if (boblight_error)
+  {
+    PrintError(boblight_error);
+    return 1;
+  }
+
+}
