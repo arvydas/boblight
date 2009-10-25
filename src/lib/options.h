61f9239aaec861e7e754f635f0598b2c2af170ae
bob.loosen@gmail.com <bob.loosen@gmail.com@c8956a1e-8086-11de-bc80-f7d40095c7a4>
2009-10-25 23:53:45 +0000

    cosmetic
    git-svn-id: http://boblight.googlecode.com/svn/trunk@206 c8956a1e-8086-11de-bc80-f7d40095c7a4

--- src/lib/options.h
+++ src/lib/options.h
@@ -16,20 +16,20 @@
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-//              name           type    min             max                default variable         post-process
-BOBLIGHT_OPTION(speed,         float,  0.0,            100.0,             100.0,  m_speed,         m_speed = Clamp(m_speed, 0.0, 100.0); send = true;)
-BOBLIGHT_OPTION(autospeed,     bool,   0,              1,                 0,      m_autospeed,     ;)
-BOBLIGHT_OPTION(interpolation, bool,   0,              1,                 0,      m_interpolation, send = true;)
-BOBLIGHT_OPTION(use,           bool,   0,              1,                 0,      m_use,           send = true;)
-BOBLIGHT_OPTION(saturation,    float,  0.0,            20.0,              1.0,    m_saturation,    m_saturation = Max(m_saturation, 0.0);)
-BOBLIGHT_OPTION(saturationmin, float,  0.0,            1.0,               0.0,    m_satrange[0],   m_satrange[0] = Clamp(m_satrange[0], 0.0, m_satrange[1]);)
-BOBLIGHT_OPTION(saturationmax, float,  0.0,            1.0,               1.0,    m_satrange[1],   m_satrange[1] = Clamp(m_satrange[1], m_satrange[0], 1.0);)
-BOBLIGHT_OPTION(value,         float,  0.0,            20.0,              1.0,    m_value,         m_value = Max(m_value, 0.0);)
-BOBLIGHT_OPTION(valuemin,      float,  0.0,            1.0,               0.0,    m_valuerange[0], m_valuerange[0] = Clamp(m_valuerange[0], 0.0, m_valuerange[1]);)
-BOBLIGHT_OPTION(valuemax,      float,  0.0,            1.0,               1.0,    m_valuerange[1], m_valuerange[1] = Clamp(m_valuerange[0], m_valuerange[0], 1.0);)
-BOBLIGHT_OPTION(threshold,     int,    0,              255,               0,      m_threshold,     m_threshold = Clamp(m_threshold, 0, 255);)
-BOBLIGHT_OPTION(hscanstart,    float,  0.0,            100.0,             -1.0,   m_hscan[0],      m_hscan[0] = Clamp(m_hscan[0], 0.0, m_hscan[1]);    SetScanRange(m_width, m_height);)
-BOBLIGHT_OPTION(hscanend,      float,  0.0,            100.0,             -1.0,   m_hscan[1],      m_hscan[1] = Clamp(m_hscan[1], m_hscan[0], 100.0);  SetScanRange(m_width, m_height);)
-BOBLIGHT_OPTION(vscanstart,    float,  0.0,            100.0,             -1.0,   m_vscan[0],      m_vscan[0] = Clamp(m_vscan[0], 0.0, m_vscan[1]);    SetScanRange(m_width, m_height);)
-BOBLIGHT_OPTION(vscanend,      float,  0.0,            100.0,             -1.0,   m_vscan[1],      m_vscan[1] = Clamp(m_vscan[1], m_vscan[0], 100.0);  SetScanRange(m_width, m_height);)
+//              name           type    min  max    default variable         post-process
+BOBLIGHT_OPTION(speed,         float,  0.0, 100.0, 100.0,  m_speed,         m_speed = Clamp(m_speed, 0.0, 100.0); send = true;)
+BOBLIGHT_OPTION(autospeed,     bool,   0,   1,     0,      m_autospeed,     ;)
+BOBLIGHT_OPTION(interpolation, bool,   0,   1,     0,      m_interpolation, send = true;)
+BOBLIGHT_OPTION(use,           bool,   0,   1,     0,      m_use,           send = true;)
+BOBLIGHT_OPTION(saturation,    float,  0.0, 20.0,  1.0,    m_saturation,    m_saturation = Max(m_saturation, 0.0);)
+BOBLIGHT_OPTION(saturationmin, float,  0.0, 1.0,   0.0,    m_satrange[0],   m_satrange[0] = Clamp(m_satrange[0], 0.0, m_satrange[1]);)
+BOBLIGHT_OPTION(saturationmax, float,  0.0, 1.0,   1.0,    m_satrange[1],   m_satrange[1] = Clamp(m_satrange[1], m_satrange[0], 1.0);)
+BOBLIGHT_OPTION(value,         float,  0.0, 20.0,  1.0,    m_value,         m_value = Max(m_value, 0.0);)
+BOBLIGHT_OPTION(valuemin,      float,  0.0, 1.0,   0.0,    m_valuerange[0], m_valuerange[0] = Clamp(m_valuerange[0], 0.0, m_valuerange[1]);)
+BOBLIGHT_OPTION(valuemax,      float,  0.0, 1.0,   1.0,    m_valuerange[1], m_valuerange[1] = Clamp(m_valuerange[0], m_valuerange[0], 1.0);)
+BOBLIGHT_OPTION(threshold,     int,    0,   255,   0,      m_threshold,     m_threshold = Clamp(m_threshold, 0, 255);)
+BOBLIGHT_OPTION(hscanstart,    float,  0.0, 100.0, -1.0,   m_hscan[0],      m_hscan[0] = Clamp(m_hscan[0], 0.0, m_hscan[1]);    SetScanRange(m_width, m_height);)
+BOBLIGHT_OPTION(hscanend,      float,  0.0, 100.0, -1.0,   m_hscan[1],      m_hscan[1] = Clamp(m_hscan[1], m_hscan[0], 100.0);  SetScanRange(m_width, m_height);)
+BOBLIGHT_OPTION(vscanstart,    float,  0.0, 100.0, -1.0,   m_vscan[0],      m_vscan[0] = Clamp(m_vscan[0], 0.0, m_vscan[1]);    SetScanRange(m_width, m_height);)
+BOBLIGHT_OPTION(vscanend,      float,  0.0, 100.0, -1.0,   m_vscan[1],      m_vscan[1] = Clamp(m_vscan[1], m_vscan[0], 100.0);  SetScanRange(m_width, m_height);)
 
