1f6d025c4662d8098533ef17aee391d2694409a8
bob.loosen@gmail.com <bob.loosen@gmail.com@c8956a1e-8086-11de-bc80-f7d40095c7a4>
2009-10-26 01:40:11 +0000

    autospeed changes
    git-svn-id: http://boblight.googlecode.com/svn/trunk@212 c8956a1e-8086-11de-bc80-f7d40095c7a4

--- src/lib/boblight-client.cpp
+++ src/lib/boblight-client.cpp
@@ -199,7 +199,7 @@ void CLight::GetRGB(float* rgb)
     if (m_autospeed)
     {
       float change = Abs(rgb[0] - m_prevrgb[0]) + Abs(rgb[1] - m_prevrgb[1]) + Abs(rgb[2] - m_prevrgb[2]);
-      m_autospeedvalue = Clamp(m_speed * 10.0 * change / 3.0, 0.0, 100.0);
+      m_autospeedvalue = Clamp(change / 3.0 * 100.0, m_speed, 100.0);
     }
 
     memcpy(m_prevrgb, rgb, sizeof(m_prevrgb));
