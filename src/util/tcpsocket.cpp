a84486b18c84e3ade8bdeba8512b112088c05b2a
bob.loosen <bob.loosen@c8956a1e-8086-11de-bc80-f7d40095c7a4>
2013-01-09 23:52:32 +0000

    fixed: include order
    git-svn-id: http://boblight.googlecode.com/svn/trunk@462 c8956a1e-8086-11de-bc80-f7d40095c7a4

--- src/util/tcpsocket.cpp
+++ src/util/tcpsocket.cpp
@@ -15,6 +15,9 @@
  * You should have received a copy of the GNU General Public License along
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+#include "inclstdint.h"
+
 #include <stdio.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
