cfafcefe1edb231ec489b1f3accd52ca944fff7a
Arvydas Juskevicius <arvydas@agileinnovative.co.uk>
2014-07-28 18:01:15 +0100

    Automatically detect OSX

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-aclocal && libtoolize && autoheader && automake --add-missing && autoconf
+if [ "$LIBTOOLIZE" = "" ] && [ "`uname`" = "Darwin" ]; then
+  LIBTOOLIZE=glibtoolize
+fi
+
+ACLOCAL=${ACLOCAL:-aclocal}
+AUTOCONF=${AUTOCONF:-autoconf}
+AUTOHEADER=${AUTOHEADER:-autoheader}
+AUTOMAKE=${AUTOMAKE:-automake}
+LIBTOOLIZE=${LIBTOOLIZE:-libtoolize}
+
+set -ex
+"$ACLOCAL"
+"$LIBTOOLIZE"
+"$AUTOHEADER"
+"$AUTOCONF"
+"$AUTOMAKE" --add-missing --copy
+
+#aclocal && libtoolize && autoheader && automake --add-missing && autoconf
